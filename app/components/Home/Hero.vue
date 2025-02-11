16a15681f0101d969ec916084e0c455ffa057189
Michael Synan <hello@michaelsynan.com>
2025-02-11 18:07:27 -0500

    Feat: added images to service pages

--- app/components/Home/Hero.vue
+++ app/components/Home/Hero.vue
@@ -4,20 +4,25 @@
     style="background-size: cover; background-position: center;">
     <div class="overlay"></div>
     <div class="grid md:grid-cols-2 grid-cols-1 gap-4 h-screen max-w-5xl mx-auto pt-[73px]">
-      <div class="flex justify-center items-center">
+      <div class="flex justify-center items-center w-[200px] md:w-[240px] mx-auto">
         <Nuxt-img src="/primo-main-logo.webp" alt="Primo Sewer Cleaning" width="240" />
       </div>
       <div class="flex flex-col justify-center items-center md:items-start gap-6">
-        <h1 class="text-5xl md:text-6xl caslon leading-tight">Plumbing & Sewer Cleaning Services <br> You Can
+        <!-- H1 for larger screens hidden on smaller screens -->
+        <h1 class="text-5xl md:text-6xl caslon leading-tight hidden md:block">Plumbing & Sewer Cleaning Services <br>
+          You Can
           Trust</h1>
-        <div class="flex flex-row gap-4">
-          <SharedButton text="Call Now" />
-          <SharedButton text="Learn More" variant="ghost" />
+        <!-- H1 for smaller screens hidden on larger screens -->
+        <h1 class="text-3xl caslon leading-snug md:hidden text-center">Plumbing & Sewer Services You Can Count On</h1>
+        <div class="flex flex-row gap-4 w-full">
+          <SharedButton text="Call Now" class="w-full" />
+          <SharedButton class="hidden md:inline" text="Learn More" variant="ghost" />
         </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <style scoped>
 .overlay {
